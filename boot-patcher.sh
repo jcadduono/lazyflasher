026dd854e28acce27a753055612e7ec230f90e60
James Christopher Adduono <jc@adduono.com>
2016-07-31 14:34:22 -0400

    Add Samsung boot image tag

--- boot-patcher.sh
+++ boot-patcher.sh
@@ -190,6 +190,13 @@ build_boot() {
 		}
 }
 
+# append Samsung enforcing tag to prevent warning at boot
+samsung_tag() {
+	if getprop ro.product.manufacturer | grep -iq '^samsung$'; then
+		echo "SEANDROIDENFORCE" >> "$tmp/boot-new.img"
+	fi
+}
+
 # write the new boot image to boot block
 write_boot() {
 	print "Writing new boot image to memory..."
@@ -218,6 +225,8 @@ patch_ramdisk
 build_ramdisk
 
 build_boot
+
+samsung_tag
 
 write_boot
 
