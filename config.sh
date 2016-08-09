d17882eaaf51128ecbc7fa50c7e97156154648bc
James Christopher Adduono <jc@adduono.com>
2016-08-09 19:18:48 -0400

    Use arch tools, enable lzop and bzip2

--- config.sh
+++ config.sh
@@ -16,3 +16,28 @@ split_img=$tmp/split-img
 
 ## end config variables
 
+case $(uname -m) in
+i*86*)
+	arch=x86
+	;;
+x*64*)
+	arch=x64
+	;;
+armv8*)
+	arch=arm64
+	;;
+armv7*)
+	arch=armv7
+	;;
+mips64*)
+	arch=mips64
+	;;
+mips*)
+	arch=mips
+	;;
+*)
+	arch=armv7
+	;;
+esac
+
+bin=$bin/$arch
