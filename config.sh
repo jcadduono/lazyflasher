ce365e42baf4b8de089db66817722badc6509da1
James Christopher Adduono <jc@adduono.com>
2016-10-19 03:14:15 -0400

    Remove unnecessary binaries

--- config.sh
+++ config.sh
@@ -7,33 +7,8 @@ bin=$tmp/tools
 ramdisk=$tmp/ramdisk
 ramdisk_patch=$ramdisk-patch
 split_img=$tmp/split-img
-# set this only if boot-patcher.sh can't find your boot partition
-boot_block=
 
 ## end config variables
 
-case $(getprop ro.product.cpu.abi) in
-x86)
-	arch=x86
-	;;
-x86_64)
-	arch=x64
-	;;
-arm64*)
-	arch=arm64
-	;;
-armeabi*)
-	arch=armv7
-	;;
-mips64)
-	arch=mips64
-	;;
-mips)
-	arch=mips
-	;;
-*)
-	arch=armv7
-	;;
-esac
-
+arch=arm64
 bin=$bin/$arch
