5661d0443fedf48a8294e9a8445cb961434cfee2
James Christopher Adduono <jc@adduono.com>
2016-10-22 18:06:47 -0400

    Additional fixes, verify boot image size, allow backups

--- config.sh
+++ config.sh
@@ -7,6 +7,10 @@ bin=$tmp/tools
 ramdisk=$tmp/ramdisk
 ramdisk_patch=$ramdisk-patch
 split_img=$tmp/split-img
+# set this only if boot-patcher.sh can't find your boot partition
+boot_block=
+# set this if you want to back up your boot partition
+# boot_backup=/data/local/boot-backup.img
 
 ## end config variables
 
